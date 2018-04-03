97ce6fe29cda76b7be3d88eb3db0d54c304c5b10
2606mika@gmail.com <mikael.borges@academico.ifrn.edu.br>
2018-04-03 10:50:31 -0300

    teste

---
+++ Mikael/Strings/php.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+    $strings = $_GET["teste"];
+    
+    ?>
+
+  </body>
+</html>
